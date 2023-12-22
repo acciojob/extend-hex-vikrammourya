afcb4ef8da7a5528b4545efe58f746fe7ba9562d
Vikram Mourya <141583397+vikrammourya@users.noreply.github.com>
2023-12-22 23:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length -3 , isCapital = false ;
+	let output ="#" ;
+	while(i<shortHex.length){
+		let ascicode = shortHex[i].charcodeAt(0);
+		if(ascicode>=65 && ascicode <= 90){
+			isCapital= true ;
+		}
+
+		output = output+shortHex[i]+shortHex[i];
+	}
+
+	return isCapital? output.toUpperCase() : output ;
+	
 };
 
 // Do not change the code below.
